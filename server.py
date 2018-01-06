2d2ff92c0f38a8063847a591a5470024370817c9
hbootwalla <hbootwalla@gmail.com>
2018-01-06 01:30:07 -0500

    Class-ed training model, added functionality to tune learning_param, epochs

--- server.py
+++ server.py
@@ -1,28 +1,40 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
-from json import dumps
-import titanic
+from titanic import Predictor 
 
 app = Flask(__name__)
 api = Api(app)
 
-class Predictor(Resource):
-    def post(self):
-        pclass = int(request.json['pclass']);
-        age = int(request.json['age']);
-        male = int(request.json['male']);
-        siblingCount = int(request.json['siblingCount']);
-        parchCount = int(request.json['parchCount']);
+class Prediction(Resource):
+    
+    def get(self):
+        pclass = int(request.args.get('pclass'));
+        age = int(request.args.get('age'));
+        male = int(request.args.get('male'));
+        siblingCount = int(request.args.get('siblingCount'));
+        parchCount = int(request.args.get('parchCount'));
         
         if (pclass == 1 or pclass == 2 or pclass == 3) and (age > 0 and age < 150) and (male == 0 or male == 1) and (siblingCount >= 0 and siblingCount < 100) and (parchCount >= 0 and parchCount < 100):  
-            prediction = titanic.test(pclass, age, male, siblingCount, parchCount);
+            
+            prediction = predictor.test(pclass, age, male, siblingCount, parchCount);
             return {'result': prediction} 
         else:
             return {'error' : 'Incorrect Input'}
             
-titanic.train();
-api.add_resource(Predictor, '/predictor') # Route_1
+    def post(self):
+        learning_rate = float(request.json['learning_rate']);
+        epochs = int(request.json['epochs']);
+        if learning_rate > 0.0:
+            predictor.set_learning_rate(learning_rate);
+        if epochs > 0:
+            predictor.set_epochs(epochs);
+        predictor.train();
+        
+      
+predictor = Predictor();
+predictor.train();      
+
+api.add_resource(Prediction, '/prediction') 
 
 
 if __name__ == '__main__':
